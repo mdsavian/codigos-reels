ecd99c66d43dc1eef939bfae63449ebcdf31bc37
mdsavian <marlon.savian@gmail.com>
2023-01-06 18:45:38 -0300

    feat: ajustes animation e App

--- exemplos-react/src/App.tsx
+++ exemplos-react/src/App.tsx
@@ -1,15 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BotaoEnviar } from "./buttons";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Exemplo de projetos feitos em React</p>
-      </header> */}
-      <BotaoEnviar />
+      </header>
     </div>
   );
 }
