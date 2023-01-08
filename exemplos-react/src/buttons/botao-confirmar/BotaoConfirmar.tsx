830b5279fe2f1eeac6550ecf4e2b934a415ae3a5
mdsavian <marlon.savian@gmail.com>
2023-01-08 16:51:29 -0300

    feat: adicionado botao confirmar

---
+++ exemplos-react/src/buttons/botao-confirmar/BotaoConfirmar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+
+import "./styles.css";
+
+const BotaoConfirmar: React.FC = () => {
+  const [estaConfirmando, setEstaConfirmando] = useState(false);
+  const [foiConfirmado, setFoiConfirmado] = useState(false);
+
+  const handleClick = () => {
+    setEstaConfirmando(true);
+
+    setTimeout(() => {
+      setEstaConfirmando(false);
+      setFoiConfirmado(true);
+
+      setTimeout(() => setFoiConfirmado(false), 2500);
+    }, 2000);
+  };
+  return (
+    <button onClick={handleClick} className={estaConfirmando || foiConfirmado ? "confirmando" : ""}>
+      <span className="icon material-symbols-outlined">done</span>
+
+      <span className="text">
+        {estaConfirmando ? "Confirmando..." : foiConfirmado ? "Confirmado" : "Confirmar"}
+      </span>
+    </button>
+  );
+};
+
+export default BotaoConfirmar;
